e1d606d03fa7e9237cb94a30681ba9833c8bc688
zotta <zotta@DESKTOP-4CGMDUF.student.uod.otago.ac.nz.>
2021-03-25 15:06:30 +1300

    Getters and setters added to the Sale class.
    #1

--- Phase I/sales-service/src/main/java/domain/Sale.java
+++ Phase I/sales-service/src/main/java/domain/Sale.java
@@ -32,6 +32,54 @@ public class Sale {
         this.uri = uri;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSaleDate() {
+        return saleDate;
+    }
+
+    public void setSaleDate(String saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Map<String, SaleItem> getItems() {
+        return items;
+    }
+
+    public void setItems(Map<String, SaleItem> items) {
+        this.items = items;
+    }
+
+    public Totals getTotals() {
+        return totals;
+    }
+
+    public void setTotals(Totals totals) {
+        this.totals = totals;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
 
     
     
