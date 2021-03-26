cd451f56e8b382c87f05dcba038d3b121f90a8fb
zotta <zotta@DESKTOP-4CGMDUF.student.uod.otago.ac.nz.>
2021-03-26 17:03:05 +1300

    get sales for a specific customer added to SaleResource.
    #1

--- Phase I/sales-service/src/main/java/resource/SaleResource.java
+++ Phase I/sales-service/src/main/java/resource/SaleResource.java
@@ -6,17 +6,40 @@
 package resource;
 
 import dao.SaleDAO;
+import domain.ErrorMessage;
 import org.jooby.Jooby;
+import org.jooby.Status;
 
 /**
  *
  * @author zotta
  */
-public class SaleResource extends Jooby{
-    
-    public SaleResource(SaleDAO dao){
-        
-        
+public class SaleResource extends Jooby {
+
+    public SaleResource(SaleDAO dao) {
+
+        path("/api/Sales/sale/customer", () -> {
+
+            // A route that sits at the top of the chain that checks that the ID
+            // is valid so that the other routes don't need to.
+            use("/:id", (req, rsp, chain) -> {
+                String id = req.param("id").value();
+
+                if (dao.exists(id)) {
+                    // ID is OK, so pass request on to the next route in the chain
+                    chain.next(req, rsp);
+                } else {
+                    // Bad ID - send a 404 response
+                    rsp.status(Status.NOT_FOUND).send(new ErrorMessage("There is no sale matching that ID."));
+                }
+            });
+
+            get("/:id", (req) -> {
+                String id = req.param("id").value();
+                return dao.getByCustomer(id);
+            });
+
+        });
+
     }
-    
 }
