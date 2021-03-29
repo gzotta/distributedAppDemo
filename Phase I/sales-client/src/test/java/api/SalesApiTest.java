81b84f7bd36aaa4e68cd4c4d36d3423a2fb4bb7d
zotta <zotta@DESKTOP-4CGMDUF.student.uod.otago.ac.nz.>
2021-03-30 12:14:31 +1300

    Client tests and YAML modified. - Tests still failing, need to fix.
    #3

--- Phase I/sales-client/src/test/java/api/SalesApiTest.java
+++ Phase I/sales-client/src/test/java/api/SalesApiTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -60,27 +61,27 @@ public class SalesApiTest {
 
         item1 = new SaleItem();
         item1.setProductId("productid1");
-        item1.setQuantity(new BigDecimal(1));
-        item1.setPrice(new BigDecimal(11.11));
+        item1.setQuantity(new BigDecimal("1.00"));
+        item1.setPrice(new BigDecimal("11.11"));
 
         item2 = new SaleItem();
         item2.setProductId("productid2");
         item2.setQuantity(new BigDecimal(2));
-        item2.setPrice(new BigDecimal(5000.22));
+        item2.setPrice(new BigDecimal("5000.22"));
 
         items.add(item1);
         items2.add(item1);
         items2.add(item2);
 
         totals1 = new Totals();
-        totals1.setTotalPrice(new BigDecimal(11.11));
-        totals1.setTotalTax(new BigDecimal(16.6));
-        totals1.setTotalPayment(new BigDecimal(12.11));
+        totals1.setTotalPrice(new BigDecimal("11.11"));
+        totals1.setTotalTax(new BigDecimal("16.66"));
+        totals1.setTotalPayment(new BigDecimal("12.11"));
 
         totals2 = new Totals();
-        totals2.setTotalPrice(new BigDecimal(10000.12));
-        totals2.setTotalTax(new BigDecimal(16.6));
-        totals2.setTotalPayment(new BigDecimal(10000.12));
+        totals2.setTotalPrice(new BigDecimal("10000.12"));
+        totals2.setTotalTax(new BigDecimal("16.66"));
+        totals2.setTotalPayment(new BigDecimal("10000.12"));
 
         customer1 = new Customer();
         customer1.setId("customerid1");
@@ -94,15 +95,15 @@ public class SalesApiTest {
 
         sale1 = new Sale();
         sale1.setId("saleid1");
-        sale1.setCustomer(customer1);
         sale1.setSaleDate("06/06/2021");
+        sale1.setCustomer(customer1);
         sale1.setItems(items);
         sale1.setTotals(totals1);
 
         sale2 = new Sale();
         sale2.setId("saleid2");
-        sale2.setCustomer(customer2);
         sale2.setSaleDate("07/06/2021");
+        sale2.setCustomer(customer2);
         sale2.setItems(items2);
         sale2.setTotals(totals2);
 
@@ -118,29 +119,58 @@ public class SalesApiTest {
     @AfterEach
     public void tearDown() throws IOException {
         saleApi.delete(sale1.getId()).execute();
+        saleApi.delete(sale2.getId()).execute();
     }
 
-    
-    
     /**
      * test addSale method of class salesApi.
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     @Test
     public void addSale() throws IOException {
-        fail("Test is not implemented yet.");
+        // call the method being tested
+        Response<Sale> createResponse = salesApi.addSale(sale2).execute();
+
+        // check for 201 response
+        // GET the sale that was created in the setUp() to check it was saved properly
+        Response<List<Sale>> getResponse = salesApi.getByCustomer(sale1.getCustomer().getId()).execute();
+
+        // get the sale from the response body
+        Object returnedSale = getResponse.body();
+
+        // check that returned sale has the correct properties (except for URI which was set by the service, so will be different)
+        assertThat(returnedSale, samePropertyValuesAs(sale1, "uri"));
+
+        // check that the URI property was properly set by service
+        assertThat(returnedSale, hasProperty("uri", equalTo("http://localhost:8081/api/sales/sale/sale1")));
+        // call the method again - should get 422 response this time
+        createResponse = salesApi.addSale(sale2).execute();
+        assertThat(createResponse.code(), is(422));
     }
 
     @Test
     public void getByCustomer() throws IOException {
-        fail("Test is not implemented yet.");
+        // call the method being tested
+        Response<List<Sale>> getResponse = salesApi.getByCustomer("customer1id").execute();
+
+        // get the products from the response
+        List<Sale> returnedSales = getResponse.body();
+
+        // check for 200 response
+        assertThat(getResponse.code(), is(200));
+
+        // the generated equals method includes the URI, so we need to set it if we want to compare products via hasItems
+        sale1.setUri("http://localhost:8081/api/sales/sale1/saleid1");
+        sale2.setUri("http://localhost:8081/api/sales/sale2/saleid2");
+
+        // check that response includes both prod1 and prod2
+        assertThat(returnedSales, hasItem(sale1));
     }
 
-    
-       @Test
+    @Test
     public void getSummary() throws IOException {
         fail("Test is not implemented yet.");
     }
-    
-    
+
 }
