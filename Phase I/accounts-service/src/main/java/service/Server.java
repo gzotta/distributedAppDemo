44a068b1c35219310af6fe57984b63891fc15562
zotta <zotta@DESKTOP-4CGMDUF.student.uod.otago.ac.nz.>
2021-03-30 17:52:18 +1300

    Accounts server added.
    #4

--- Phase I/accounts-service/src/main/java/service/Server.java
+++ Phase I/accounts-service/src/main/java/service/Server.java
@@ -1,29 +1,36 @@
 package service;
 
+import dao.AccountDAO;
 import java.io.IOException;
 import org.jooby.Jooby;
 import org.jooby.handlers.Cors;
 import org.jooby.handlers.CorsHandler;
 import org.jooby.json.Gzon;
+import resource.AccountResource;
+import resource.AccountsResource;
 
 public class Server extends Jooby {
 
-	public Server() {
+    public Server() {
 
-		// the port that the service will run on (should be different for each service)
-		port(8081);
+        // the port that the service will run on (should be different for each service)
+        port(8080);
 
-		// add CORS support so the client can access the operations
-		use("*", new CorsHandler(new Cors().withMethods("*")));
+        // add CORS support so the client can access the operations
+        use("*", new CorsHandler(new Cors().withMethods("*")));
 
-		// encode message bodies as JSON
-		use(new Gzon());
+        // encode message bodies as JSON
+        use(new Gzon());
 
-	}
+        AccountDAO dao = new AccountDAO();
+        use(new AccountResource(dao));
+        use(new AccountsResource(dao));
 
-	public static void main(String[] args) throws IOException {
-		// start the service
-		new Server().start();
-	}
+    }
+
+    public static void main(String[] args) throws IOException {
+        // start the service
+        new Server().start();
+    }
 
 }
