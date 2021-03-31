672d7aeffbff7e7ef5e61eebbf48894fab6ded25
zotta <zotta@DESKTOP-4CGMDUF.student.uod.otago.ac.nz.>
2021-03-31 21:42:33 +1300

    SalesApiTest fixed and passed.
    #3

--- Phase I/sales-client/src/test/java/api/SalesApiTest.java
+++ Phase I/sales-client/src/test/java/api/SalesApiTest.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Multimap;
 import domain.Customer;
 import domain.Sale;
 import domain.SaleItem;
+import domain.Summary;
 import domain.Totals;
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -43,12 +44,12 @@ public class SalesApiTest {
     private Sale sale1;
     private Sale sale2;
 
-    private Customer customer1;
-    private Customer customer2;
+    private Customer customerTest1;
+    private Customer customerTest2;
 
-    private  static final List<SaleItem> items = new ArrayList<>();
-    private  static final List<SaleItem> items2 = new ArrayList<>();
-    private static final Multimap<String, Sale> customerSales = ArrayListMultimap.create();
+    private static List<SaleItem> items;
+    private static List<SaleItem> items2;
+    private static Multimap<String, Sale> customerSales;
 
     @BeforeEach
     public void setUp() throws IOException {
@@ -61,13 +62,16 @@ public class SalesApiTest {
 
         item1 = new SaleItem();
         item1.setProductId("productid1");
-        item1.setQuantity(new BigDecimal("1.00"));
+        item1.setQuantity(new BigDecimal("1.0"));
         item1.setPrice(new BigDecimal("11.11"));
 
         item2 = new SaleItem();
         item2.setProductId("productid2");
-        item2.setQuantity(new BigDecimal(2));
+        item2.setQuantity(new BigDecimal("2.0"));
         item2.setPrice(new BigDecimal("5000.22"));
+
+        items = new ArrayList<>();
+        items2 = new ArrayList<>();
 
         items.add(item1);
         items2.add(item1);
@@ -83,32 +87,34 @@ public class SalesApiTest {
         totals2.setTotalTax(new BigDecimal("16.66"));
         totals2.setTotalPayment(new BigDecimal("10000.12"));
 
-        customer1 = new Customer();
-        customer1.setId("customerid1");
-        customer1.setEmail("email1");
-        customer1.setGroup("Regular Customers");
+        customerTest1 = new Customer();
+        customerTest1.setId("customerid11");
+        customerTest1.setEmail("email1");
+        customerTest1.setGroup("Regular Customers");
 
-        customer2 = new Customer();
-        customer2.setId("customerid2");
-        customer2.setEmail("email2");
-        customer2.setGroup("Regular Customers");
+        customerTest2 = new Customer();
+        customerTest2.setId("customerid22");
+        customerTest2.setEmail("email2");
+        customerTest2.setGroup("Regular Customers");
 
         sale1 = new Sale();
         sale1.setId("saleid1");
         sale1.setSaleDate("06/06/2021");
-        sale1.setCustomer(customer1);
+        sale1.setCustomer(customerTest1);
         sale1.setItems(items);
         sale1.setTotals(totals1);
 
         sale2 = new Sale();
         sale2.setId("saleid2");
         sale2.setSaleDate("07/06/2021");
-        sale2.setCustomer(customer2);
+        sale2.setCustomer(customerTest2);
         sale2.setItems(items2);
         sale2.setTotals(totals2);
 
-        customerSales.put("customerid1", sale1);
-        customerSales.put("customerid2", sale2);
+        customerSales = ArrayListMultimap.create();
+
+        customerSales.put("customerid11", sale1);
+        customerSales.put("customerid22", sale2);
 
         // POST sale1 to service (leave sale2 for other testing the POST method itself)
         salesApi.addSale(sale1).execute();
@@ -134,45 +140,56 @@ public class SalesApiTest {
 
         // check for 201 response
         assertThat(createResponse.code(), is(201));
-        
-//        // GET the sale that was created in the setUp() to check it was saved properly
-//        Response<List<Sale>> getResponse = salesApi.getByCustomer(sale1.getCustomer().getId()).execute();
-//
-//        // get the sale from the response body
-//        List<Sale> returnedSale = getResponse.body();
-//
-//        // check that returned sale has the correct properties (except for URI which was set by the service, so will be different)
-//        assertThat(returnedSale, samePropertyValuesAs(sale1, "uri"));
-//
-//        // check that the URI property was properly set by service
-//        assertThat(returnedSale, hasProperty("uri", equalTo("http://localhost:8081/api/sales/sale/sale1")));
-//        // call the method again - should get 422 response this time
-//        createResponse = salesApi.addSale(sale2).execute();
-//        assertThat(createResponse.code(), is(422));
+
+        // GET the sale that was created in the setUp() to check it was saved properly
+        Response<Sale> getResponse = salesApi.getById(sale1.getId()).execute();
+
+        // get the sale from the response body
+        Sale returnedSale = getResponse.body();
+
+        // check that returned sale has the correct properties (except for URI which was set by the service, so will be different)
+        assertThat(returnedSale, samePropertyValuesAs(sale1, "uri"));
+
+        // check that the URI property was properly set by service
+        assertThat(returnedSale, hasProperty("uri", equalTo("http://localhost:8081/api/sales/sale/saleid1")));
+        // call the method again - should get 422 response this time
+        createResponse = salesApi.addSale(sale2).execute();
+        assertThat(createResponse.code(), is(422));
     }
 
-//    @Test
-//    public void getByCustomer() throws IOException {
-//        // call the method being tested
-//        Response<List<Sale>> getResponse = salesApi.getByCustomer("customer1id").execute();
-//
-//        // get the products from the response
-//        List<Sale> returnedSales = getResponse.body();
-//
-//        // check for 200 response
-//        assertThat(getResponse.code(), is(200));
-//
-//        // the generated equals method includes the URI, so we need to set it if we want to compare products via hasItems
-//        sale1.setUri("http://localhost:8081/api/sales/sale1/saleid1");
-//        sale2.setUri("http://localhost:8081/api/sales/sale2/saleid2");
-//
-//        // check that response includes both prod1 and prod2
-//        assertThat(returnedSales, hasItem(sale1));
-//    }
+    @Test
+    public void getByCustomer() throws IOException {
+        // call the method being tested
+        Response<List<Sale>> getResponse = salesApi.getByCustomer(customerTest1.getId()).execute();
+
+        // get the products from the response
+        List<Sale> returnedSales = getResponse.body();
+
+        // check for 200 response
+        assertThat(getResponse.code(), is(200));
+
+        // the generated equals method includes the URI, so we need to set it if we want to compare products via hasItems
+        sale1.setUri("http://localhost:8081/api/sales/sale/saleid1");
+
+        // check that response includes both prod1 and prod2
+        assertThat(returnedSales, hasItems(sale1));
+    }
 
     @Test
     public void getSummary() throws IOException {
-        fail("Test is not implemented yet.");
+        // call the method being tested
+        Response<Summary> getResponse = salesApi.getSummary(customerTest1.getId()).execute();
+
+        // get the products from the response
+        Summary returnedSummary = getResponse.body();
+
+        // check for 200 response
+        assertThat(getResponse.code(), is(200));
+
+        // check that the URI property was properly set by service
+        assertThat(returnedSummary, hasProperty("numberOfSales", equalTo(1)));
+        assertThat(returnedSummary, hasProperty("totalPayment", equalTo(new BigDecimal("12.11"))));
+        assertThat(returnedSummary, hasProperty("group", equalTo("Regular Customers")));
     }
 
 }
